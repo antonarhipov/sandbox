902d149d56ecf85a49ff9a31ca47d988b7c9ba13
Anton Arhipov <ant.arhipov@gmail.com>
2019-12-11 01:09:57 +0200

    clean state

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,4 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
@@ -5,6 +6,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.version
 version = "2019.2"
 
 project {
+    vcsRoot(ProjectVcs)
 
 }
 
@@ -31,3 +33,9 @@ object Test3 : BuildType({
 object Deploy : BuildType({
     name = "Deploy"
 })
+
+object ProjectVcs : GitVcsRoot({
+    name = "ProjectVcs"
+    url = "git://some.git.repository"
+})
+
