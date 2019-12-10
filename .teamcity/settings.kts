5bc62e54ed3e9ac36efa7928468458a458678879
Anton Arhipov <ant.arhipov@gmail.com>
2019-12-11 01:14:52 +0200

    new project pipeline

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,13 +1,29 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
+import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 version = "2019.2"
 
 project {
     vcsRoot(ProjectVcs)
+    buildType(Config)
+    val sequence = sequential {
+        buildType(Build)
+        parallel {
+            buildType(Test1)
+            sequential {
+                buildType(Test2)
+                buildType(Test3)
+            }
 
+            dependsOn(Config)
+        }
+        buildType(Deploy)
+    }
+
+    sequence.buildTypes().forEach(this::buildType)
 }
 
 object Build : BuildType({
