04165a22d65b6feb195c8415ba067b96b3e6ff93
Anton Arhipov <ant.arhipov@gmail.com>
2019-12-11 00:58:06 +0200

    clean state

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,29 +1,11 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
 import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 version = "2019.2"
 
 project {
-    buildType(Config)
-    val sequence = sequential {
-        buildType(Build)
 
-        parallel {
-            buildType(Test1) {
-                runOnSameAgent = true
-            }
-            buildType(Test2)
-            buildType(Test3)
-
-            dependsOn(Config)
-        }
-
-        buildType(Deploy)
-    }
-
-    sequence.buildTypes().forEach(this::buildType)
 }
 
 object Build : BuildType({
