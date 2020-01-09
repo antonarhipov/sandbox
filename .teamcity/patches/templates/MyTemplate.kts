ff0e3dbfcecb017e4f474080367a6a5f389694f2
admin <admin@TeamCity>
2020-01-09 22:44:23 +0200

    TeamCity change in 'Sandbox' project: VCS roots of 'MyTemplate' build template were updated

--- .teamcity/patches/templates/MyTemplate.kts
+++ .teamcity/patches/templates/MyTemplate.kts
@@ -15,6 +15,10 @@ create(DslContext.projectId, Template({
     id("MyTemplate")
     name = "MyTemplate"
 
+    vcs {
+        root(RelativeId("ProjectVcs"))
+    }
+
     steps {
         script {
             id = "RUNNER_3"
