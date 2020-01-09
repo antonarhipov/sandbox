af4e30dd2a906228e0379e498aa7ccf2fa2daa1f
Anton Arhipov <ant.arhipov@gmail.com>
2020-01-09 23:56:02 +0200

    experimenting

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,56 +1,47 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
-import jetbrains.buildServer.configs.kotlin.v2019_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2019_2.project
-import jetbrains.buildServer.configs.kotlin.v2019_2.sequential
-import jetbrains.buildServer.configs.kotlin.v2019_2.version
 
 version = "2019.2"
 
 project {
     vcsRoot(ProjectVcs)
 
-    buildType(Config)
-    val sequence = sequential {
-        buildType(Build)
-        parallel {
-            buildType(Test1)
-            buildType(Test2)
-
-            dependsOn(Config)
-        }
-        buildType(Deploy)
-    }
-
-    sequence.buildTypes().forEach(this::buildType)
-
+    buildType(BuildA)
+    buildType(BuildB)
 }
-
-object Build : BuildType({
-    name = "Build"
-})
-
-object Config : BuildType({
-    name = "Config"
-})
-
-object Test1 : BuildType({
-    name = "Test1"
-})
-
-object Test2 : BuildType({
-    name = "Test2"
-})
-
-object Test3 : BuildType({
-    name = "Test3"
-})
-
-object Deploy : BuildType({
-    name = "Deploy"
-})
 
 object ProjectVcs : GitVcsRoot({
     name = "ProjectVcs"
     url = "git://some.git.repository"
 })
 
+//---------------------------------------------------------------------------
+
+open class MyCommonTemplate : BuildType({
+    steps {
+        script {
+            scriptContent = """echo "Hello from base template" """
+        }
+    }
+})
+
+object BuildA : MyCommonTemplate() {
+    init {
+        steps {
+            script {
+                scriptContent = """echo "More steps for A" """
+            }
+        }
+    }
+}
+
+object BuildB : MyCommonTemplate() {
+    init {
+        steps {
+            script {
+                scriptContent = """echo "More steps for B" """
+            }
+        }
+    }
+}
